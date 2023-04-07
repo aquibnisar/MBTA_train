dacc0f3f6b641eaa62b3dab29fe100cdd780b9d7
Mohd Aquib <mohd.aquib@scoreme.in>
2023-04-08 01:59:19 +0530

    changes

--- src/main/java/com/train/mbta/utility/ConstantVariables.java
+++ src/main/java/com/train/mbta/utility/ConstantVariables.java
@@ -2,6 +2,6 @@ package com.train.mbta.utility;
 
 public class ConstantVariables {
 
-	public static final String GET_TRAIN_DATA_URL = "/developers/v3-api";
+	public static final String GET_TRAIN_DATA_URL = "/getTrainData";
 	
 }
